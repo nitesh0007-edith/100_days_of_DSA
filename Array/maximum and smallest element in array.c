0103e19c7d3185eeddbe17ef6136345d57c90c31
nitesh0007-edith <63156061+nitesh0007-edith@users.noreply.github.com>
2021-04-18 11:58:33 +0530

    Add files via upload

---
+++ Array/maximum and smallest element in array.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+main()
+{
+    int arr[10],i,n,max,min;
+    printf("Enter size of the array:");
+    scanf("%d",&n);
+
+    printf("enter the element in array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    max=arr[0];
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+
+    min=arr[0];
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]<max)
+        {
+            min=arr[i];
+        }
+    }
+
+    printf("Maximum element in array:%d",max);
+    printf("\nminimum element in array:%d",min);
+
+}
