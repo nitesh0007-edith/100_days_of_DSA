2679bebb463e056231838900d75207d64c7a26c8
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-04-24 15:01:22 +0530

    Create calloc() by gog.c

---
+++ Dynamic Memory Allocation(DMA)/calloc()/calloc() by gog.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int* ptr,i,n;
+    printf("enter size of arr:");
+    scanf("%d",&n);
+    printf("Element of array:");
+    ptr =(int *) calloc(n,sizeof(int));
+    if(ptr==NULL)
+    {
+        printf("out of memory error:");
+        exit(0);
+    }
+    else
+    {
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&ptr[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",ptr[i]);
+    }
+    return 0;
+}
