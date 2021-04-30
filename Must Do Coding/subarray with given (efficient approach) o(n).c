452ff4a7560cc206b41d34862349fe0bafe6429b
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-04-30 19:34:56 +0530

    Create subarray with given (efficient approach) o(n).c

---
+++ Must Do Coding/subarray with given (efficient approach) o(n).c
@@ -0,0 +1,38 @@
+// Time complexity = O(n)
+#include<stdio.h>
+
+void subArraySum(int arr[],int len,int sum)
+{
+
+    for(int i=0;i<len;i++)
+    {
+        int current_sum=arr[i];
+        int j=i+1;
+        while(j<=len)
+        {
+            if(current_sum==sum)
+            {
+                printf("%d %d",i+1,(j-1)+1);
+                return 1;
+            }
+            if(current_sum>sum || j==len)
+            {
+                break;
+            }
+            current_sum+=arr[j];
+            j++;
+        }
+    }
+    printf("No Sub array Found:\n");
+}
+
+int main()
+{
+    int arr[]={1,4,20,3,10,5};
+    int sum=33;
+    int len = sizeof(arr) / sizeof(arr[0]);
+    subArraySum(arr,len,sum);
+
+    return 0;
+
+}
