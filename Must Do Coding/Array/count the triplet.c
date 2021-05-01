be01fa46c648b9532d12a261e6f648be4ab4f3cf
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-01 15:40:08 +0530

    Add files via upload

---
+++ Must Do Coding/Array/count the triplet.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]={1,5,3,2};
+    int n=sizeof(arr) / sizeof(arr[0]);
+    int count=0;
+    for(int i=0;i<n-2;i++)
+    {
+
+        for(int j=i+1;j<n-1;j++)
+        {
+
+            for(int k=j+1;k<n;k++)
+            {
+
+                if(arr[i]+arr[j]==arr[k] || arr[j]+arr[k]==arr[i] || arr[k]+arr[i]==arr[j])
+                {
+                    count++;
+                }
+            }
+        }
+
+    }
+    printf("%d ",count);
+    return 0;
+
+}
