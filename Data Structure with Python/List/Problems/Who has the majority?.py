5e06bbcff3ee92ba2d5ea8835fc8a71f313cc5b4
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-10 18:00:27 +0530

    Create Who has the majority?.py

---
+++ Data Structure with Python/List/Problems/Who has the majority?.py
@@ -0,0 +1,15 @@
+def majorityWins(arr, n, x, y):
+        # code here
+        xcount=0
+        ycount=0
+        for i in arr:
+            if i == x:
+                xcount+=1
+            elif i == y:
+                ycount+=1
+        if xcount > ycount:
+            return x
+        elif xcount==ycount:
+            return min(x,y)
+        else:
+            return y
