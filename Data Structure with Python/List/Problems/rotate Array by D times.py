7fd4f7154b297df9d5c29c9f3fe37b2c1425f994
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-10 18:55:04 +0530

    Create rotate Array by D times.py

---
+++ Data Structure with Python/List/Problems/rotate Array by D times.py
@@ -0,0 +1,12 @@
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self,A,D,N):
+        self.reverse(A,0,D-1)
+        self.reverse(A,D,N-1)
+        self.reverse(A,0,N-1)
+        
+    def reverse(self,A,start,end):
+        while(start<end):
+            A[start],A[end]=A[end],A[start]
+            start+=1
+            end-=1
