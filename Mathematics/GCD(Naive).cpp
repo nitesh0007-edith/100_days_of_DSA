013560a7c81c32c83fa0b8411ea10114ccad7c14
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-18 19:06:50 +0530

    Create GCD(Naive).cpp

---
+++ Mathematics/GCD(Naive).cpp
@@ -0,0 +1,26 @@
+//this is the Naive Solution
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int GCD(int a,int b)
+{
+	int result=min(a,b);
+	while(result>0)
+	{
+		if(a%result==0 && b%result==0)
+		{
+			break;
+		}
+		result--;
+	}
+	return result;
+	
+}
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<GCD(a,b);
+	
+	return 0;
+}
