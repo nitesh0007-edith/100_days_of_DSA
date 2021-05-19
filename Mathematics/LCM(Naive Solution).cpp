c7080af189051582c2ac1b931029720102f3e5f3
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-19 18:13:50 +0530

    Create LCM(Naive Solution).cpp

---
+++ Mathematics/LCM(Naive Solution).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<limits.h>
+
+using namespace std;
+
+int LCM(int a,int b)
+{
+	int res = max(a,b);
+	while(true)
+	{
+		if(res%a==0 && res%b==0)
+		{
+			return res;
+		}
+		res++;
+	}
+	return res;
+}
+int main()
+{
+	int num1,num2;
+	cin>>num1>>num2;
+	cout<<LCM(num1,num2);
+	
+	return 0;
+}
