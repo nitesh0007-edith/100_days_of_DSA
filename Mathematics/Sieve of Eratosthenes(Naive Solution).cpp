5da00b0baf42963b2ed4fd9aa7ff407803ea3b5d
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-22 17:53:53 +0530

    Create Sieve of Eratosthenes(Naive Solution).cpp

---
+++ Mathematics/Sieve of Eratosthenes(Naive Solution).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+bool isprime(int);
+void SieveOfEratosthencs(int);
+
+void SieveOfEratosthencs(int n)
+{
+	for(int i=2;i<=n;i++)
+	{
+		if(isprime(i))
+		{
+			cout<<i<<" ";
+		}
+	}	
+}
+
+bool isprime(int n)
+{
+	if (n==1)
+	{
+		return false;
+	}
+	for(int i=2;i*i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			return false;
+		}
+	}
+	return true;
+} 
+int main()
+{
+	int num;
+	cin>>num;
+	SieveOfEratosthencs(num);
+	
+	return 0;
+}
