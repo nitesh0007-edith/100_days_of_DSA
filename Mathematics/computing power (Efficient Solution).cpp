7a86b79fd552149f3a55e2b84bdfd9949329faa7
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-22 20:20:04 +0530

    Create computing power (Efficient Solution).cpp

---
+++ Mathematics/computing power (Efficient Solution).cpp
@@ -0,0 +1,27 @@
+// this is recursive solution 
+// so time complexity = 0(log n)
+// but auxilary space = 0(log n)
+#include<iostream>
+#include<limits.h>
+
+using namespace std;
+
+int Power(int x,int n)
+{
+	if(n==0)
+		return 1;
+	int temp=Power(x,n/2);
+	temp=temp*temp;
+	if(n%2==0)
+		return temp;
+	else
+		return temp*x;	
+}
+int main()
+{
+	int x,n;
+	cin>>x>>n;
+	cout<<Power(x,n);
+	
+	return 0;
+}
