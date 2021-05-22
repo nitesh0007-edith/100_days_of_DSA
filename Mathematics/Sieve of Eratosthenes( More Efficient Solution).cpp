1f2accb17717081f55a15df40872584feb4a4025
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-22 19:13:07 +0530

    Create Sieve of Eratosthenes( More Efficient Solution).cpp

---
+++ Mathematics/Sieve of Eratosthenes( More Efficient Solution).cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+
+using namespace std;
+
+void SieveOfEratosthencs(int n)
+{
+	vector<bool> isprime(n+1,true);
+	
+	for (int i=2;i*i<=n;i++)
+	{
+		if(isprime[i])
+		{
+			for(int j=i*i;j<=n;j=j+i)
+			{
+				isprime[j]=false;
+			}
+		}
+	}
+	for(int i=2;i<=n;i++)
+	{
+		if(isprime[i])
+		{
+			cout<<i<<" ";
+		}
+	}
+}
+int main()
+{
+	int num;
+	cin>>num;
+	SieveOfEratosthencs(num);
+	
+	return 0;
+}
