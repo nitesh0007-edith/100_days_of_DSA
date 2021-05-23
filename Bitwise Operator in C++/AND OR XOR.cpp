47dfbf40ecc0b131375c09d66bf24642629a5222
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-23 16:43:01 +0530

    Create AND OR XOR.cpp

---
+++ Bitwise Operator in C++/AND OR XOR.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+int main()
+{
+	int x=3,y=6;
+	cout<<(x&y)<<endl; // Bitwise AND
+	cout<<(x|y)<<endl; // Bitwise OR
+	cout<<(x^y)<<endl; // Bitwise XOR
+	
+	return 0;
+}
