acd30ae780aa3b850b4bcf016eb0c4331acb63ad
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-25 02:04:33 +0530

    Create count set bit(Naive solution).cpp

---
+++ Bitwise Operator in C++/count set bit(Naive solution).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<limits.h>
+
+using namespace std;
+
+int count_set_bit(int n)
+{
+	int count=0;
+	while(n>0)
+	{
+	
+		/*if((n&1)==1);
+		{
+			count++;
+		}*/
+		count=count+(n&1);
+		n=n>>1;
+	}
+	return count;
+}
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<count_set_bit(n);
+	
+	return 0;
+}
