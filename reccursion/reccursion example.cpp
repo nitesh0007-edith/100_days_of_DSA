0081e9c41fbffdd5df0ebf575ae5f2058d7bd31b
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-05-30 14:58:17 +0530

    Create reccursion example.cpp

---
+++ reccursion/reccursion example.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+
+void fun1()
+{
+	cout<<"fun1\n";
+}
+void fun2()
+{
+	cout<<"Before fun1\n";
+	fun1();
+	cout<<"After fun1\n";
+}
+int main()
+{
+	cout<<"Before fun2\n";
+	fun2();
+	cout<<"After fun2\n";
+	return 0;
+}
