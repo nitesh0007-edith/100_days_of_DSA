5fe46953076031da68038dcd59ea3b302aa382b4
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-06-02 16:05:33 +0530

    Create Tower of Hanoi.cpp

---
+++ reccursion/Tower of Hanoi.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void TOH(int n,char A,char B,char C)
+{
+	if(n==1)
+	{
+		cout<<"Move 1 From "<<A<<" To "<<C<<endl;
+		return;
+	}
+	TOH(n-1,A,C,B);
+	cout<<"Move "<<n<<" From "<<A<<" To "<<C<<endl;
+	TOH(n-1,B,A,C);
+}
+int main()
+{
+	int n=3;
+	TOH(n,'A','B','C');
+	return 0;
+}
