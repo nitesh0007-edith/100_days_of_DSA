19bbfac5ca2b1caa33a42fc091c23bcaa0ba0058
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-06-04 20:42:31 +0530

    Create search(Unsorted array).cpp

---
+++ Array/search(Unsorted array).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int searching(int arr[],int n,int x)
+{
+	for (int i=0;i<n;i++)
+	{
+		if(arr[i]==x)
+			return i;
+	}
+	return -1;
+}
+int main()
+{
+	int arr[]={20,5,7,25};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int x=5;
+	cout<<searching(arr,n,x);
+	return 0;
+}
