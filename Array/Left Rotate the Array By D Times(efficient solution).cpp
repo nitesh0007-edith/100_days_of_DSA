15f28013f38c001959beef101cda14ec6fe20802
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-06-06 17:16:16 +0530

    Create Left Rotate the Array By D Times(efficient solution).cpp

---
+++ Array/Left Rotate the Array By D Times(efficient solution).cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+void reverse(int arr[],int start,int end);
+
+void RotateByDtimes(int arr[],int n,int d)
+{
+	reverse(arr,0,d-1);
+	reverse(arr,d,n-1);
+	reverse(arr,0,n-1);
+}
+void reverse(int arr[],int start,int end)
+{
+	while(start<end)
+	{
+			swap(arr[start],arr[end]);
+			start++;
+			end--;	
+	}
+}
+
+
+int main()
+{
+	int arr[]={1,2,3,4,5,6};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int d;
+	cin>>d;
+	RotateByDtimes(arr,n,d);
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
