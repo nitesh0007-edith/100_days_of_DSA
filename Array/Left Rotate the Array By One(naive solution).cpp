4cf783d241a611ceaa555cc6e2650ae54a71fa8a
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-06-06 17:14:10 +0530

    Create Left Rotate the Array By One(naive solution).cpp

---
+++ Array/Left Rotate the Array By One(naive solution).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void RotateByOne(int arr[],int n)
+{
+	int temp=arr[0];
+	for(int i=1;i<n;i++)
+	{
+		arr[i-1]=arr[i];
+	}
+	arr[n-1]=temp;
+}
+int main()
+{
+	int arr[]={1,2,3,4,5,6};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	RotateByOne(arr,n);
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
