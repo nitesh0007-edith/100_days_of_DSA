90beb797f9ab3c005c91cdd4e8f309f02ea90d4d
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-06-17 22:47:08 +0530

    Create Index of Last Index Occurence(Recursive solution).cpp

---
+++ Searching/Index of Last Index Occurence(Recursive solution).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int LasttOcc(int arr[],int low,int high,int x)
+{
+	if(low>high)
+	{
+		return -1;
+	}
+	int mid=(low+high)/2;
+	if(arr[mid]>x)
+	{
+		return FirstOcc(arr,low,mid-1,x);
+	}
+	else if(arr[mid]<x)
+	{
+		return LastOcc(arr,mid+1,high,x);
+	}
+	else
+	{
+		if(mid==high || arr[mid+1]!=arr[mid])
+		{
+			return mid;
+		}
+		else
+		{
+			return LastOcc(arr,mid+1,high,x);
+		}
+	}
+}
+int main()
+{
+	int arr[]={1,10,10,10,20,20,40};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int x=10;
+	cout<<LastOcc(arr,0,n-1,x);
+	return 0;
+}
