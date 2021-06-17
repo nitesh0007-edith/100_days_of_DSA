0871a8e3406ef2c7946cd3a8814195d249820b75
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-06-17 22:44:40 +0530

    Create Index of First Index Occurrence(Iterative solution).cpp

---
+++ Searching/Index of First Index Occurrence(Iterative solution).cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int FirstOcc(int arr[],int n,int x)
+{
+	int low=0,high=n-1;
+	while(low<=high)
+	{
+		int mid=(low+high)/2;
+		if(arr[mid]>x)
+		{
+			high=mid-1;
+		}
+		else if(arr[mid]<x)
+		{
+			low=mid+1;
+		}
+		else
+		{
+			if(mid==0 || arr[mid-1]!=arr[mid])
+			{
+				return mid;
+			}
+			else
+			{
+				high=mid-1;
+			}
+		}
+		
+	}
+	return -1;
+}
+int main()
+{
+	int arr[]={1,10,10,10,20,20,40};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int x=20;
+	cout<<FirstOcc(arr,n,x);
+	return 0;
+}
