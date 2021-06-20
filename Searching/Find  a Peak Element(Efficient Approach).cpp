d379f2a4e5d0b58ed29b148f4b9bc74aef3bcefb
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-06-20 21:55:08 +0530

    Create Find  a Peak Element(Efficient Approach).cpp

---
+++ Searching/Find  a Peak Element(Efficient Approach).cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int Peak(int arr[],int n)
+{
+	int low=0,high=n-1;
+	while(low<=high)
+	{
+		int mid=(low+high)/2;
+		if( mid==0 || arr[mid]>=arr[mid-1] && mid==n-1 || arr[mid]>=arr[mid+1] )
+		{
+			return arr[mid];
+		}
+		if(mid>0 && arr[mid] <= arr[mid-1] )
+		{
+			high=mid-1;
+		}
+		else
+		{
+			low=mid+1;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	int arr[]={10,20,15,5,23,90,67};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	cout<<Peak(arr,n);
+	return 0;
+}
