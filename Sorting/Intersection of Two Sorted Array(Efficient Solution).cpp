268e89ef729522d7c2747bed30a2283798cfc52f
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-07-07 11:57:22 +0530

    Create Intersection of Two Sorted Array(Efficient Solution).cpp

---
+++ Sorting/Intersection of Two Sorted Array(Efficient Solution).cpp
@@ -0,0 +1,39 @@
+//Time Complexity :- O(m+n)
+#include<iostream>
+using namespace std;
+
+void intersection(int a[],int b[],int m,int n)
+{
+	int i=0,j=0;
+	while(i<m && j<n)
+	{
+		if(i>0 && a[i]==a[i-1])
+		{
+			i++;
+			continue;
+		}
+		if(a[i] < b[j])
+		{
+			i++;
+		}
+		else if(a[i] > b[j])
+		{
+			j++;
+		}
+		else
+		{
+			cout<<a[i]<<" ";
+			i++;
+			j++;
+		}
+	}
+}
+int main()
+{
+	int a[]={3,5,10,10,15,15,20};
+	int m=sizeof(a)/sizeof(a[0]);
+	int b[]={5,10,10,15,30};
+	int n=sizeof(b)/sizeof(b[0]);
+	intersection(a,b,m,n);
+	return 0;
+}
