91b4350bacd5f29bcad026dce6663fdedf2a75b0
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-07-10 16:22:26 +0530

    Create sort an array of 0s,1s and 2s (using Dutch National Flag Algorithms).cpp

---
+++ Sorting/sort an array of 0s,1s and 2s (using Dutch National Flag Algorithms).cpp
@@ -0,0 +1,36 @@
+// Time Complexity : 0(n), Aux Space
+#include<iostream>
+using namespace std;
+
+void Sort(int arr[],int n)
+{
+	int l=0,h=n-1,mid=0;
+	while(mid<=h)
+	{
+		if(arr[mid]==0)
+		{
+			swap(arr[l],arr[mid]);
+			l++;
+			mid++;
+		}
+		else if(arr[mid]==1)
+		{
+			mid++;
+		}
+		else
+		{
+			swap(arr[mid],arr[h]);
+			h--;
+		}
+	}
+}
+int main()
+{
+	int arr[]={0,1,1,2,2,1,0,0,2,1,1,0,2};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	Sort(arr,n);
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+}
