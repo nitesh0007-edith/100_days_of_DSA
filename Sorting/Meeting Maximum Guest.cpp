9e0280f83bbdb7eaec54f83c1be5cac20fad1ac3
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-07-11 17:05:17 +0530

    Create Meeting Maximum Guest.cpp

---
+++ Sorting/Meeting Maximum Guest.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int MaxGuest(int arr[],int dep[],int n)
+{
+	sort(arr,arr+n);
+	sort(dep,dep+n);
+	int i=1,j=0,res=1,curr=1;
+	while(i<n && j<n)
+	{
+		if(arr[i]<=dep[j])
+		{
+			curr++;
+			i++;
+		}
+		else
+		{
+			curr--;
+			j++;
+		}
+		res=max(res,curr);
+	}
+	return res;
+}
+int main()
+{
+	int arr[]={900,600,700};
+	int dep[]={1000,800,730};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	cout<<MaxGuest(arr,dep,n);
+	return 0;
+}
