d8d50d903ca0c72835be3c113980c11792672c1f
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-07-12 16:24:39 +0530

    Create Cycle Sort(Distinct Element).cpp

---
+++ Sorting/Cycle Sort(Distinct Element).cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void CycleSort(int arr[],int n)
+{
+	for(int cs=0;cs<n-1;cs++)
+	{
+		int item=arr[cs];
+		int pos=cs;
+		for(int i=cs+1;i<n;i++)
+		{
+			if(arr[i]<item)
+				pos++;
+		}
+		swap(item,arr[pos]);
+		while(pos!=cs)
+		{
+			pos=cs;
+			for(int i=cs+1;i<n;i++)
+			{
+				if(arr[i]<item)
+					pos++;
+			}
+			swap(item,arr[pos]);
+		}
+	}
+}
+int main()
+{
+	int arr[]={20,40,50,10,30};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	CycleSort(arr,n);
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
