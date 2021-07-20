1066e485365105d2baea9432327a33dc1caa6199
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2021-07-20 16:19:17 +0530

    Create Linked List Implementation.cpp

---
+++ Linked List/Linked List Implementation.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *next;
+	Node(int x)
+	{
+		data=x;
+		next=NULL;
+	}
+};
+int main()
+{
+	Node *head = new Node(10);
+	Node *temp1 = new Node(20);
+	Node *temp2 = new Node(30);
+	head->next=temp1;
+	temp1->next=temp2;
+	cout<<head->data<<"->"<<temp1->data<<"->"<<temp2->data;
+	// short implementation
+	// head -> next = New Node(20);			
+	// head -> next -> next = New Node(30);
+	return 0;
+}
