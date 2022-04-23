35a6b78e452eb514b2999ff82db78af90f091321
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2022-04-23 21:08:31 +0530

    Create Time Conversion.py

---
+++ Hackerrank/Time Conversion.py
@@ -0,0 +1,10 @@
+def timeConversion(s):
+    # Write your code here
+    if s[-2:]=="AM" and s[:2]=="12":
+        return "00"+s[2:-2]
+    elif s[-2:]=="AM":
+        return s[:-2]
+    elif s[-2:]=="PM" and s[:2]=="12":
+        return s[:-2]
+    else:
+        return str(int(s[:2])+12)+s[2:-2]
